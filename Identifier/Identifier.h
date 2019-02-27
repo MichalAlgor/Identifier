efa60af88ce1c8ce4b589df105a4570d24db7cfc
Michal Czupryna <michal.czupryna@twoupdigital.com>
2019-02-27 10:24:10 +0100

    Initial Commit

---
+++ Identifier/Identifier.h
@@ -0,0 +1,19 @@
+//
+//  Identifier.h
+//  Identifier
+//
+//  Created by Michal Czupryna on 27/02/2019.
+//  Copyright © 2019 Michal Czupryna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Identifier.
+FOUNDATION_EXPORT double IdentifierVersionNumber;
+
+//! Project version string for Identifier.
+FOUNDATION_EXPORT const unsigned char IdentifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Identifier/PublicHeader.h>
+
+
